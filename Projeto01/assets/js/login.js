ef9f774d49fc4455f3bdd985791058d160ffc8f8
marioeug <74729160+marioeug@users.noreply.github.com>
2021-05-25 16:19:49 -0300

    alteração de codigo
    Foi feito melhorias no HTML para consistir entrada dos campos nas telas

--- Projeto01/assets/js/login.js
+++ Projeto01/assets/js/login.js
@@ -1,8 +1,18 @@
-function validar() {
+function validarcampos() {
     const nomemail = document.getElementById("fname");
+    const nomesenha = document.getElementById("fname2");
     if (nomemail.value === "" || nomemail.value === null) {
-        return alert("Preencha o campo com o e-mail");
+        alert("Campo obrigatório. Preencha com o valor do email");
+        window.setTimeout(function() {
+            nomemail.focus();
+        }, 0);
+    } else if (nomesenha.value === "" || nomesenha.value === null) {
+        alert("Campo obrigatório. Preencha com o valor da senha");
+        window.setTimeout(function() {
+            nomesenha.focus();
+        }, 0);
     } else {
-        return true;
+        return (window.location.href =
+            "C:/Dados/GitHUB/CodandoTalentos/Projeto01/index.html");
     }
 }
