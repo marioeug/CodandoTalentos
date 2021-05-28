b7d58995be4f0540a7c8c9334aefdf746cc894b3
marioeug <74729160+marioeug@users.noreply.github.com>
2021-05-28 13:39:59 -0300

    implementado menagem padrao
    modificado o sistema de alerta de mensagens no exercicio

--- Projeto01/assets/js/login.js
+++ Projeto01/assets/js/login.js
@@ -1,38 +1,38 @@
-function validarcampos() {
-    const nomemail = document.getElementById("fname");
-    const nomesenha = document.getElementById("fname2");
-    if (nomemail.value === "" || nomemail.value === null) {
-        alert("Campo obrigatório. Preencha com o valor do email");
-        window.setTimeout(function() {
-            nomemail.focus();
-        }, 0);
-    } else if (nomesenha.value === "" || nomesenha.value === null) {
-        alert("Campo obrigatório. Preencha com o valor da senha");
-        window.setTimeout(function() {
-            nomesenha.focus();
-        }, 0);
-    } else {
-        return (window.location.href =
-            "C:/Dados/GitHUB/CodandoTalentos/Projeto01/index.html");
-    }
-}
 // function validarcampos() {
 //     const nomemail = document.getElementById("fname");
 //     const nomesenha = document.getElementById("fname2");
 //     if (nomemail.value === "" || nomemail.value === null) {
-//         Swal.fire({
-//             icon: "error",
-//             title: "Oops...",
-//             text: "Campo obrigatório. Preencha com o valor do email"
-//         });
+//         alert("Campo obrigatório. Preencha com o valor do email");
+//         window.setTimeout(function() {
+//             nomemail.focus();
+//         }, 0);
 //     } else if (nomesenha.value === "" || nomesenha.value === null) {
-//         Swal.fire({
-//             icon: "error",
-//             title: "Oops...",
-//             text: "Campo obrigatório. Preencha com o valor da senha"
-//         });
+//         alert("Campo obrigatório. Preencha com o valor da senha");
+//         window.setTimeout(function() {
+//             nomesenha.focus();
+//         }, 0);
 //     } else {
 //         return (window.location.href =
 //             "C:/Dados/GitHUB/CodandoTalentos/Projeto01/index.html");
 //     }
 // }
+function validarcampos() {
+    const nomemail = document.getElementById("fname");
+    const nomesenha = document.getElementById("fname2");
+    if (nomemail.value === "" || nomemail.value === null) {
+        Swal.fire({
+            icon: "error",
+            title: "Gafio...",
+            text: "Campo obrigatório. Preencha o campo de email"
+        });
+    } else if (nomesenha.value === "" || nomesenha.value === null) {
+        Swal.fire({
+            icon: "error",
+            title: "Gafio...",
+            text: "Campo obrigatório. Preencha o campo de senha"
+        });
+    } else {
+        return (window.location.href =
+            "C:/Dados/GitHUB/CodandoTalentos/Projeto01/index.html");
+    }
+}
